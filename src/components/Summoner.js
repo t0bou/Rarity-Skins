b9885a05e1e7d231d6bf467bd805a4101a2f3c5c
t0bou <hyperstalker.z4@gmail.com>
2021-10-13 00:36:07 +0200

    added infos feature

--- src/components/Summoner.js
+++ src/components/Summoner.js
@@ -1,4 +1,4 @@
-import { useContractCall, useContractFunction } from "@usedapp/core"
+import { useContractCall } from "@usedapp/core"
 import { ethers } from "ethers"
 import * as addresses from "../addresses.json"
 import * as summonerSkinsJson from "../artifacts/contracts/SummonerSkins.sol/SummonerSkins.json"
@@ -6,10 +6,10 @@ import * as managerJson from "../artifacts/contracts/RaritySkinManager.sol/Rarit
 import * as rarityABI from "../dependencies/RarityABI.json"
 import Loading from "./Loading"
 import emptyImg from "../dependencies/empty.png"
-import { Card, Button } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import LoadingGif from "../dependencies/loading.gif"
 
-export default function Summoner({id, managerAddress, price}){
+export default function Summoner({id, managerAddress}){
     const managerABI = JSON.stringify(managerJson.abi)
     const managerInterface = new ethers.utils.Interface(managerABI)
     const skinStruct = useContractCall({abi : managerInterface, address: managerAddress[0], method: "skinOf", args: [id]})
@@ -17,20 +17,16 @@ export default function Summoner({id, managerAddress, price}){
     return(
         <>
             {!skinStruct && <Loading/>}
-            {skinStruct && <_Summoner id={id} price={price} skinId={skinStruct[1].toNumber()} />}
+            {skinStruct && <_Summoner id={id} skinId={skinStruct[1].toNumber()} />}
         </>
     )
 }
 
-function _Summoner({id, skinId, price}){
+function _Summoner({id, skinId}){
     const rarityAddress = '0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb'
     const classes = ["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Wizard"]
     const rarityInterface = new ethers.utils.Interface(JSON.stringify(rarityABI.default))
     const summonerClass = useContractCall({abi: rarityInterface, address: rarityAddress, method: 'class', args: [id.toString()]})
-    const skinsABI = JSON.stringify(summonerSkinsJson.abi)
-    const skinsInterface = new ethers.utils.Interface(skinsABI)
-    const skinContract = new ethers.Contract(addresses.summonerSkins,skinsInterface)
-    const mint = useContractFunction(skinContract,'mintAndAssign')
 
     return(
         <>
@@ -46,7 +42,6 @@ function _Summoner({id, skinId, price}){
                             {skinId === 0 && <><br/>no skin</>}
                             {skinId !== 0 && <><br/>Rare Skin #{skinId}</>}
                         </Card.Text>
-                        <Button size="sm" onClick={()=>{mint.send([id],{value: price.toString()})}}>Mint a new skin !</Button>
                     </Card.Body>
                 </Card>
             </>}
